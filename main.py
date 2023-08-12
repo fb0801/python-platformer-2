909b4de29709a4fcc03a5ae996550ff564ef3883
fb0801 <fbhatti0801@gmail.com>
2023-08-12 17:42:04 +0100

    generate the bg

--- main.py
+++ main.py
@@ -10,15 +10,38 @@ pygame.init()
 
 pygame.display.set_caption('Platformer')
 
-BG_COLOR = (255,255,255)
+
 WIDTH, HEIGHT = 1000, 800
 FPS = 60
 PLAYER_VEL = 5
 
-window = pygame.set_mode((WIDTH, HEIGHT))
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+
+def get_background(name):
+    image = pygame.image.load(join("assets", "Background", name))
+   
+    _,_, width, height = image.get_rect()
+    tiles=[]
+
+    for i in range(WIDTH // width + 1):
+        for j in range(HEIGHT // height + 1):
+            pos = (i * width, j* height)
+            tiles.append(pos)
+
+    return tiles, image
+
+def draw(window, background, bg_image):
+    #draw onto screen and update display
+    for tile in background:
+        window.blit(bg_image, tile)
+
+    pygame.display.update()
+
+    
 
 def main(window):
-    clock = pygame.time.CLock()
+    clock = pygame.time.Clock()
+    background, bg_image = get_background('Blue.png')
 
     run = True
     while run:
@@ -28,6 +51,8 @@ def main(window):
             if event.type == pygame.QUIT:
                 run = False
                 break
+
+        draw(window, background, bg_image)
     pygame.QUIT()
     quit()
 
